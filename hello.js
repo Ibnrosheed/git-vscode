ff58dd471ba6251b7dc2ce6e6a9e724343e305b9
ibnrosheed <ibnrosheed9@gmail.com>
2026-03-16 15:22:46 +0100

    test for score

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
 console.log("Hello my people");
+
+let score = 100;
+if (score > 90) {
+    console.log("You got an A");
+} else if (score > 80) {
+    console.log("You got a B");
+} else if (score > 70) {
+    console.log("You got a C");
+} else if (score > 60) {
+    console.log("You got a D");
+} else {
+    console.log("You got an F");
+}  
+
